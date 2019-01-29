41dd434d104983fe0ebf0032ff17c23ec7085f4e
oguimbal <olivier.guimbal@gadz.org>
2019-01-29 14:58:22 +0100

    fix

--- projects/ngx-material-opiniated/src/services.ts
+++ projects/ngx-material-opiniated/src/services.ts
@@ -6,7 +6,7 @@ export abstract class INotificationService {
   abstract info(message: string);
 
   abstract alert(text: string, title?: string): Promise<any>;
-  abstract confirm(message?: string): Promise<boolean>;
+  abstract confirm(message?: string, title?: string): Promise<boolean>;
   abstract saveOrDiscard(text: string, title?: string): Promise<'save' | 'discard' | 'cancel'>;
   abstract prompt(text: string, title?: string, initialText?: string, selectedText?: string): Promise<string>;
   abstract confirmWithText(text: string, title?: string, confirmText?: string);
