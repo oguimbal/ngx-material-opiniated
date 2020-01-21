415c2aad1d7d16b95691e824801222d5827d0b64
oguimbal <olivier.guimbal@gadz.org>
2020-01-21 19:30:55 +0100

    upgrade ng

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { delay } from '@oguimbal/utilities';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.pug',
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
